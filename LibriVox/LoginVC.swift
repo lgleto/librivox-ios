4e403e648d29f82404b99dc226a80779ecde852f
InstinctPt <51703514+InstinctPt@users.noreply.github.com>
2023-03-23 11:08:00 +0000

    login brain

--- LibriVox/LoginVC.swift
+++ LibriVox/LoginVC.swift
@@ -17,10 +17,7 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Auth.auth().signIn(withEmail: email.text ?? "", password: password.text ?? "") { [weak self] authResult, error in
-          guard let strongSelf = self else { return }
-          // ...
-        }
+
                 
         
 
@@ -28,6 +25,32 @@ class LoginVC: UIViewController {
     }
     
 
+    @IBAction func loginButton(_ sender: UIButton) {
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (authResult, error) in
+          if let error = error as? NSError {
+            switch AuthErrorCode(error.code) {
+            case .operationNotAllowed:
+                print("operation not allowed")
+              // Error: Indicates that email and password accounts are not enabled. Enable them in the Auth section of the Firebase console.
+            case .userDisabled:
+                print("User is disable")
+              // Error: The user account has been disabled by an administrator.
+            case .wrongPassword:
+                print("Wrong password")
+              // Error: The password is invalid or the user does not have a password.
+            case .invalidEmail:
+                print("Invalid Email")
+              // Error: Indicates the email address is malformed.
+            default:
+                print("Error: \(error.localizedDescription)")
+            }
+          } else {
+            print("User signs in successfully")
+            let userInfo = Auth.auth().currentUser
+            let email = userInfo?.email
+          }
+        }
+    }
     /*
     // MARK: - Navigation
 
