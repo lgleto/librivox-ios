8049194a56d89af7b1a02e136c7c472094fa7f8d
IceQueen <85301917+XIceQueenX@users.noreply.github.com>
2023-05-01 22:55:06 +0100

    add logoutFunction, add customView blueTextField, correct the name of green color

---
+++ LibriVox/CustomViews/BlueShadowUITextField.swift
@@ -0,0 +1,41 @@
+//
+//  BlueShadowUITextField.swift
+//  LibriVox
+//
+//  Created by Acesso Gloria MP on 01/05/2023.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class BlueShadowUITextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    private var shadowLayer: CAShapeLayer!
+    private let cornerRadius: CGFloat = 10
+    private let fillColor: UIColor = UIColor(red: 252.0, green: 252.0, blue: 252.0, alpha: 1.0)
+    private let shadowColor: UIColor = UIColor(named: "Main Blue Tone")!
+    private let shadowOpacity: Float = 0.5
+    private let shadowOffset = CGSize(width: 4, height: 4)
+    private let shadowRadius: CGFloat = 0
+    
+    private func setupView() {
+        backgroundColor = fillColor
+        
+        layer.cornerRadius = cornerRadius
+        layer.shadowColor = shadowColor.cgColor
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowOffset = shadowOffset
+        layer.shadowRadius = shadowRadius
+    }
+}
--- LibriVox/ViewControllers/Profile/ProfileVC.swift
+++ LibriVox/ViewControllers/Profile/ProfileVC.swift
@@ -10,38 +10,27 @@ import FirebaseStorage
 import FirebaseAuth
 
 class ProfileVC: UIViewController {
-
+    
     @IBOutlet weak var profilePhoto: CircularImageView!
     @IBOutlet weak var nicknameUser: UILabel!
     @IBOutlet weak var nameUser: UILabel!
     
+    @IBOutlet weak var logoutBtn: UILabel!
     var name = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        let storageRef = Storage.storage().reference()
-        let imageRef = storageRef.child("images/\(Auth.auth().currentUser!.uid)/userPhoto")
-
-        imageRef.getData(maxSize: 5 * 1024 * 1024) { data, error in
-            if let error = error {
-                print("Error downloading image: \(error.localizedDescription)")
-                self.profilePhoto.image =  self.imageWith(name: self.name)
-            } else {
-                if let imageData = data {
-                    self.profilePhoto.image =  UIImage(data: imageData)
-                } else {
-                    self.profilePhoto.image =  self.imageWith(name: self.name)
-                }
-            }
-        }
         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapFunction))
+        logoutBtn.addGestureRecognizer(tap)
         
         getNameOrUserName("name") { name in
             if let name = name {
                 self.nameUser.text = name
                 self.name = name
             }
+            
+            self.downloadImage(self.name, self.profilePhoto)
         }
         
         getNameOrUserName("username") { userName in
@@ -49,47 +38,86 @@ class ProfileVC: UIViewController {
                 self.nicknameUser.text = userName
             }
         }
+        
+    }
+    
+    @IBAction func tapFunction(sender: UITapGestureRecognizer) {
+        logoutUser()
     }
     
     @IBAction func switchTheme(_ sender: UISwitch) {
         if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-            let firstWindow = windowScene.windows.first {
-
+           let firstWindow = windowScene.windows.first {
+            
             let currentTheme = firstWindow.overrideUserInterfaceStyle
             let newTheme: UIUserInterfaceStyle = currentTheme == .dark ? .light : .dark
             firstWindow.overrideUserInterfaceStyle = newTheme
         }
-
+        
     }
     
+    func downloadImage(_ name: String, _ imageView: UIImageView) {
+        let storageRef = Storage.storage().reference()
+        let imageRef = storageRef.child("images/\(Auth.auth().currentUser!.uid)/userPhoto")
+        
+        let defaultImage = imageWith(name: name)
+        imageView.image = defaultImage
+        
+        imageRef.getData(maxSize: 5 * 1024 * 1024) { data, error in
+            if let error = error {
+                print("Error downloading image: \(error.localizedDescription)")
+                    imageView.image = defaultImage
+            } else {
+                if let imageData = data {
+                    imageView.image = UIImage(data: imageData)
+                } else {
+                    imageView.image = defaultImage
+                }
+            }
+        }
+    }
+
     
     func imageWith(name: String?) -> UIImage? {
-            let frame = CGRect(x: 0, y: 0, width: 200, height: 200)
-            let nameLabel = UILabel(frame: frame)
-            nameLabel.textAlignment = .center
-            nameLabel.backgroundColor = .lightGray
-            nameLabel.textColor = .white
-            nameLabel.font = UIFont.boldSystemFont(ofSize: 64)
-            var initials = ""
-            if let initialsArray = name?.components(separatedBy: " ") {
-                if let firstWord = initialsArray.first {
-                    if let firstLetter = firstWord.first {
-                        initials += String(firstLetter).capitalized }
-                }
-                if initialsArray.count > 1, let lastWord = initialsArray.last {
-                    if let lastLetter = lastWord.first { initials += String(lastLetter).capitalized
-                    }
-                }
-            } else {
-                return nil
+        let frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        let nameLabel = UILabel(frame: frame)
+        nameLabel.textAlignment = .center
+        nameLabel.backgroundColor = UIColor(named: "Green Tone")
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 64)
+        
+        var initials = ""
+        if let initialsArray = name?.components(separatedBy: " ") {
+            if let firstWord = initialsArray.first {
+                if let firstLetter = firstWord.first {
+                    initials += String(firstLetter).capitalized }
             }
-            nameLabel.text = initials
-            UIGraphicsBeginImageContext(frame.size)
-            if let currentContext = UIGraphicsGetCurrentContext() {
-                nameLabel.layer.render(in: currentContext)
-                let nameImage = UIGraphicsGetImageFromCurrentImageContext()
-                return nameImage
+            if initialsArray.count > 1, let lastWord = initialsArray.last {
+                if let lastLetter = lastWord.first { initials += String(lastLetter).capitalized
+                }
             }
+        } else {
             return nil
         }
+        
+        nameLabel.text = initials
+        UIGraphicsBeginImageContext(frame.size)
+        if let currentContext = UIGraphicsGetCurrentContext() {
+            nameLabel.layer.render(in: currentContext)
+            let nameImage = UIGraphicsGetImageFromCurrentImageContext()
+            return nameImage
+        }
+        return nil
+    }
+    
+    func logoutUser() {
+        do { try Auth.auth().signOut() }
+        catch { print("Already logged out") }
+        
+        let storyBoard : UIStoryboard = UIStoryboard(name: "LoginRegister", bundle: nil)
+        let vc = storyBoard.instantiateViewController(withIdentifier: "LoginId") as! LoginVC
+        self.present(vc, animated: true, completion: nil)
+                
+    }
+    
 }
